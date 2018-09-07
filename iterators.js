195f447b2a7fe0c6e722aca5975d02bbbff8c35e
gtsofa <jlsnyule@yahoo.com>
2018-09-07 21:45:53 +0300

    add map method

--- iterators.js
+++ iterators.js
@@ -1,3 +1,4 @@
+// The forEach() Method
 /**
  * forEach() takes an argument of callback function. Remember, a callback function is a function passed as an argument into another function.
  * forEach() loops through the array and executes the callback function for each element. During each execution, the current element is passed as an argument to the callback function.
@@ -8,11 +9,20 @@ The return value for .forEach() will always be undefined.
 const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterate over fruits below
-fruits.forEach(function(fruitItem)
-{
-  console.log('--' + fruitItem);
-});
+// fruits.forEach(function(fruitItem)
+// {
+//   console.log('I want to eat a' + fruitItem);
+// });
 
 // using arrow function syntax
 fruits.forEach(fruitItem =>
-    console.log(fruitItem));
+    console.log('I want to eat a ' + fruitItem));
+
+// The .map() Method
+const numbers = [1, 2, 3, 4, 5]; 
+
+const bignumbers = numbers.map(number => {
+  return number * 10;
+});
+console.log(numbers);
+console.log(bignumbers);
