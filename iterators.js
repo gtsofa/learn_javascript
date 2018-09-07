ace4618d9e3a6238702d63865ea0d94edd4e4778
gtsofa <jlsnyule@yahoo.com>
2018-09-07 23:03:21 +0300

    iterators

--- iterators.js
+++ iterators.js
@@ -26,3 +26,58 @@ const bignumbers = numbers.map(number => {
 });
 console.log(numbers);
 console.log(bignumbers);
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => {
+  return animal[0];
+});
+
+console.log(secretMessage.join('')); // HelloWorld
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number => {
+  return number / 100 ;
+});
+
+console.log(smallNumbers); // [1,2,3,4,5]
+
+// The .filter() Method
+/*Another useful iterator method is .filter(). Like .map(), .filter() returns a new array. However, .filter() returns an array of elements after filtering out certain elements from the original array. The callback function for the .filter() method should return true or false depending on the element that is passed to it. The elements cause the callback function to return true are added to the new array. */
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(number => {
+  return number < 250;
+});
+console.log(smallNumbers); 
+
+// The .findIndex() Method
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+console.log(foundAnimal); // 7
+
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal[0] === 's';
+});
+
+console.log(foundAnimal);// 3
+
+// The .reduce() Method
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+console.log('The value of accumulator: ', accumulator);
+console.log('The value of currentValue: ', currentValue);
+return accumulator + currentValue;
+});
+console.log(newSum);
